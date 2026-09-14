ddc2a70674a8684c613101f417b266e94bb0cc85
nhanzh <nhannanzahirah@gmail.com>
2026-09-15 03:38:18 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -27,12 +27,13 @@ import {
 
 // Gantikan dengan kunci API Firebase Projek anda
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY_HERE",
-    authDomain: "pintar-sphere.firebaseapp.com",
-    projectId: "pintar-sphere",
-    storageBucket: "pintar-sphere.appspot.com",
-    messagingSenderId: "1234567890",
-    appId: "1:1234567890:web:abcdef123456"
+  apiKey: "AIzaSyBHuWIV-V65gyxxnWuP_qAIGljvoO5sJcs",
+  authDomain: "pintar-sphere.firebaseapp.com",
+  projectId: "pintar-sphere",
+  storageBucket: "pintar-sphere.firebasestorage.app",
+  messagingSenderId: "829082367809",
+  appId: "1:829082367809:web:50defae0288fee399cd3c0",
+  measurementId: "G-WLE5FH87LE"
 };
 
 const app = initializeApp(firebaseConfig);
