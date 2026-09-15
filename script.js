939aaac34d8834aed8b89b41fd823cc6405fd24d
nhanzh <nhannanzahirah@gmail.com>
2026-09-15 19:22:12 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+script.js function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+}
+
+window.onload = function () {
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark-mode");
+    }
+}
+
+// ========================================
+// PINTAR@Sphere WELCOME INTRO
+// ========================================
+
+window.addEventListener("load", function () {
+
+    const welcomeScreen =
+        document.getElementById("welcomeScreen");
+
+    if (!welcomeScreen) {
+        return;
+    }
+
+    setTimeout(function () {
+
+        welcomeScreen.classList.add("hide");
+
+    }, 2500);
+
+
+    setTimeout(function () {
+
+        welcomeScreen.remove();
+
+    }, 3300);
+
+});
